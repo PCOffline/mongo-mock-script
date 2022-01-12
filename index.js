2c493bd602c5d465eccca774171a2edeb2d39842
PCOffline <bekerman122@gmail.com>
2022-01-12 22:33:16 +0200

    feat: debug log all paths

--- index.js
+++ index.js
@@ -92,6 +92,23 @@ function logDebugData() {
   logDebug('Log Level:', logLevel);
   logDebug('Mongo URI:', config.mongoUri);
   logDebug('Config:', JSON.stringify(config));
+
+  const paths = await Promise.all(
+    Object.keys(config.collections).map((collectionName) => ({
+      [collectionName]: await import(
+        config.collections[collectionName].path
+      ).catch((error) => {
+        logDebug(error);
+        logError(
+          `Path '${config.collections[collectionName].path}' is invalid!`,
+        );
+
+        return 'Invalid path';
+      }),
+    })),
+  );
+
+  logDebug('Paths:', JSON.stringify(paths));
 }
 
 async function initialise() {
