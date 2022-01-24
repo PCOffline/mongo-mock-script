c8a28938dec7d081b825da569769a202356de633
PCOffline <bekerman122@gmail.com>
2022-01-24 22:54:56 +0200

    chore(config): set config values

--- config.js
+++ config.js
@@ -1,29 +1,19 @@
-import mongoose from 'mongoose';
-
-const { Schema } = mongoose;
-
 export default {
-  noPrompt: false, // Don't prompt before deleting
+  noPrompt: true, // Don't prompt before deleting
   preferPath: true, // When both path and data are provided, ignore the data and only use the path. If false, only the data will be used in such case.
   logLevel: 'info', // 'debug' | 'info' | 'warn' | 'error' | 'silent'
+  // TODO: Change to CosmosDB URI locally (or use ENV instead)
   mongoUri: 'mongodb://localhost:27017/my-database',
   collections: {
-    collectionName: {
-      schema: new Schema({
-        // The mongoose schema of the collection
-        fieldName: {
-          type: Schema.Types.String,
-          required: true,
-        },
-      }),
-      model: 'modelName', // The name of the model to use, will be equal to the collection name by default, use it in order to not break refs
-      data: [
-        // The data to be inserted into the collection
-        {
-          fieldName: 'value',
-        },
-      ],
-      path: 'path/to/file.json', // alternative to 'data', can be either json or js with default export
-    },
+    events: { path: './collections/events.js' },
+    malshabs: { path: './collections/malshabs.js' },
+    nodegroups: { path: './collections/nodegroups.js' },
+    nodes: { path: './collections/nodes.js' },
+    questionnaireinstances: { path: './collections/questionnaireinstances.js' },
+    questionnaireschemas: { path: './collections/questionnaireschemas.js' },
+    // sms: { path: './collections/sms.js' }, // TODO: Add after sms-track goes into dev
+    temporaryinstances: { path: './collections/temporaryinstances.js' },
+    units: { path: './collections/units.js' },
+    users: { path: './collections/users.js' },
   },
 };
