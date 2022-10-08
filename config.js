c047ad84b3f5d4f6cb41ed890b04b57df3725e53
PCOffline <bekerman122@gmail.com>
2022-10-08 23:29:54 +0300

    chore: update config to match readme

--- config.js
+++ config.js
@@ -5,8 +5,9 @@ const { Schema } = mongoose;
 export default {
   noPrompt: false, // Don't prompt before deleting
   preferPath: true, // When both path and data are provided, ignore the data and only use the path. If false, only the data will be used in such case.
-  logLevel: 'info', // 'debug' | 'info' | 'warn' | 'error' | 'silent'
-  mongoUri: 'mongodb://localhost:27017/my-database',
+  logLevel: 'debug', // 'debug' | 'info' | 'warn' | 'error' | 'silent'
+  mongoUri: 'mongodb://localhost:27017/my-database', // Can also be retrieved from environment variable: MONGO_URI, MONGODB_URI, DB_URI, DATABASE_URI or mongoUri
+  sensitiveDebugLog: false, // Log information that is considered sensitive (such as the MongoDB URI) in debug mode.
   collections: {
     collectionName: {
       schema: new Schema({
@@ -23,7 +24,7 @@ export default {
           fieldName: 'value',
         },
       ],
-      path: 'path/to/file.json', // alternative to 'data', can be either json or js with default export
+      path: 'path/to/file.json', // alternative to 'data', can be either .json, .js or .mjs with default export
     },
   },
 };
